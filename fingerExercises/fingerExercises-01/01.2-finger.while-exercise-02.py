1bb87e7e90240a92c39ed735ca7291e3a0a64d41
Brashare <52320738+sodaPhix@users.noreply.github.com>
2019-10-06 22:38:24 -0600

    Answer Script - While Exercise 02

---
+++ fingerExercises/fingerExercises-01/01.2-finger.while-exercise-02.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  6 20:34:16 2019
+
+@author: sodatab
+
+MITx: 6.00.1x
+"""
+
+"""
+01.2-Finger While Exercise 02
+----------------------------
+2. Convert the following into code that uses a while loop.
+
+prints Hello!
+prints 10
+prints 8
+prints 6
+prints 4
+prints 2
+"""
+
+"""Answer Script:"""
+num = 12
+
+print('Hello!')
+while num > 2:
+    num -= 2
+    print(num)
