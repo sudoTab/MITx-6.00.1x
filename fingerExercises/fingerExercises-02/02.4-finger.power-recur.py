41b1a5871a6f45ec1cd995d9572d5e223136cb5a
Brashare <52320738+sodaPhix@users.noreply.github.com>
2019-10-07 11:17:28 -0600

    Answer Script - Power Recursion

---
+++ fingerExercises/fingerExercises-02/02.4-finger.power-recur.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  7 02:17:15 2019
+
+@author: sodatab
+
+MITx: 6.00.1x
+"""
+
+"""
+02.4-Finger Power Recursion
+------------------------
+In Problem 1, we computed an exponential by iteratively executing successive multiplications. 
+We can use the same idea, but in a recursive function.
+
+Write a function recurPower(base, exp) which computes baseexp by recursively calling itself to solve a smaller version of the same problem, 
+and then multiplying the result by base to solve the initial problem.
+
+This function should take in two values - 
+base can be a float or an integer; exp will be an integer ≥0. It should return one numerical value. 
+Your code must be recursive - use of the ** operator or looping constructs is not allowed.
+"""
+
+"""Answer Script:"""
+def recurPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+
+    returns: int or float, base^exp
+    '''
+    if exp==0:
+        return 1
+    else:
+        return base*recurPower(base, exp-1)
