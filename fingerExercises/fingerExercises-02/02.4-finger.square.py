858b2eb6bf9bdf99d8d5b77163029fa66761b7c6
Brashare <52320738+sodaPhix@users.noreply.github.com>
2019-10-07 11:20:56 -0600

    Answer Script - Square Function

---
+++ fingerExercises/fingerExercises-02/02.4-finger.square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  7 01:43:41 2019
+
+@author: sodatab
+
+MITx: 6.00.1x
+"""
+
+"""
+02.4-Finger Square Function
+-------------------
+Write a Python function, square, that takes in one number and returns the square of that number.
+
+This function takes in one number and returns one number. 
+"""
+
+"""Answer Script:"""
+def square(x):
+    '''
+    x: int or float.
+    '''
+    return x**2
