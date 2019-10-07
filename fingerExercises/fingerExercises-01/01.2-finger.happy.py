71cbeeac4229dd49800ba9679c9972ebb97d5d9c
Brashare <52320738+sodaPhix@users.noreply.github.com>
2019-10-07 00:24:00 -0600

    Answer Script - happy

---
+++ fingerExercises/fingerExercises-01/01.2-finger.happy.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  6 22:43:17 2019
+
+@author: sodatab
+
+MITx: 6.00.1x
+"""
+
+"""
+01.2-Finger happy
+------------------
+Write a piece of Python code that prints out the string 
+
+    'hello world' 
+
+if the value of an integer variable, happy, is strictly greater than 2.
+
+Note:
+    
+    Assume that variables are defined for you by our grader code. 
+    
+    Simply write code assuming you know the values of the variables.
+"""
+
+"""Answer Script"""
+if happy > 2:
+    print ('hello world')
