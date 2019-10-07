3d816ac2707bdea62f167f5ce92742e7b85f7210
Brashare <52320738+sodaPhix@users.noreply.github.com>
2019-10-06 22:34:53 -0600

    Answer Script - hello world

---
+++ fingerExercises/fingerExercises-01/01.2-finger.hello-world.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  6 22:26:17 2019
+
+@author: sodatab
+
+MITx: 6.00.1x
+"""
+
+"""
+01.2-Finger hello world
+------------------------
+Write a piece of Python code that prints out the string hello world
+"""
+
+"""Answer Script"""
+print ('hello world')
